85ea18c5c33330da2a20dae8b90f531548921cdd
toswm128 <chominsu10@gmail.com>
2021-02-23 01:40:10 +0900

    [UPDATE] transparent div above the flex class

--- portfolio.js
+++ portfolio.js
@@ -1,31 +1,39 @@
-const body = document.querySelector("body");
+const body = document.querySelector(".body");
 const flex = document.querySelector(".flex");
 const id1 = document.querySelector(".id1");
 const id5 = document.querySelector(".id5");
 const HIDNE = "hiden"
 const WIDTH = "width"
+let Left = false;
+let Right = false;
 
 left = () =>{
-    id1.classList.add(WIDTH);
-    id5.classList.remove(WIDTH);
-    console.log("left")
+    if(Left){
+        id1.classList.add(WIDTH);
+        id5.classList.remove(WIDTH);
+        console.log("left")
+    }
 }
 
 right = () =>{
-    id5.classList.add(WIDTH);
-    id1.classList.remove(WIDTH);
-    console.log("right")
+    if(Right){
+        id5.classList.add(WIDTH);
+        id1.classList.remove(WIDTH);
+        console.log("right")
+    }
 }
 
 mouseMove = (e) =>{
     const mouseX = e.offsetX;
     if(mouseX>=1200){
-        console.log("오른",mouseX)
-        right()
-
-    }else if(mouseX<=400){
-        console.log("왼")
+        Left = true;
+        Right = false
         left()
+        
+    }else if(mouseX<=400){
+        Left = false;
+        Right = true
+        right()
     }else {
         console.log(mouseX)
     }
